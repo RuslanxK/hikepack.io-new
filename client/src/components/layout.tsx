3c939248d8bf680e8906c206fa53e42a9a53d7b9
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2025-04-14 16:33:53 +0300

    changes

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useLocation } from "react-router-dom";
-import { SidebarProvider} from "@/components/ui/sidebar";
+import { SidebarProvider, useSidebar } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 import Container from "./ui/container";
 import { RecentItemsBar } from "./recent-items-bar";
+import { AlignJustify, X } from "lucide-react";
 
+const MobileHeader = () => {
+  const { openMobile, toggleSidebar } = useSidebar();
+
+  return (
+    <div className="p-4 md:hidden flex items-center justify-between bg-white dark:bg-dark border-b z-50">
+      <img src="/logo-black.png" width={85} alt="Logo" />
+      <button onClick={toggleSidebar}>
+        {openMobile ? <X /> : <AlignJustify />}
+      </button>
+    </div>
+  );
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const location = useLocation();
+
   const hideSidebarRoutes = ["/login", "/register", "/error", "/reset-password", "/welcome"];
-  const shareRoutes = ["/share/", "/verify-account/", "/new-password/"]; 
+  const shareRoutes = ["/share/", "/verify-account/", "/new-password/"];
 
   const isShareRoute = shareRoutes.some((route) => location.pathname.startsWith(route));
   const shouldHideSidebar = hideSidebarRoutes.includes(location.pathname) || isShareRoute;
@@ -18,14 +32,23 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <SidebarProvider>
-      <div className="flex h-screen w-full">
-        {!shouldHideSidebar && <AppSidebar />}
-        <div className="overflow-auto bg-light dark:bg-dark w-full">
-          {shouldHideSidebar ? children : <Container>{children}</Container>}
+      <Fragment>
+        <div className="flex h-screen w-full flex-col md:flex-row relative">
+          {/* Mobile Header */}
+          <MobileHeader />
+
+          {/* Sidebar (handled internally for both mobile Sheet and desktop sidebar) */}
+          {!shouldHideSidebar && <AppSidebar />}
+
+          {/* Main Content */}
+          <div className="overflow-auto bg-light dark:bg-dark w-full">
+            {shouldHideSidebar ? children : <Container>{children}</Container>}
+          </div>
+
+          <Toaster />
+          {isBagRoute && <RecentItemsBar />}
         </div>
-        <Toaster />
-        {isBagRoute && < RecentItemsBar />}
-      </div>
+      </Fragment>
     </SidebarProvider>
   );
 }
